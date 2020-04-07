e18e5ed2861d9a290bb026d085022e88a4508d8d
Prahlad-K <prahladsharmak99@gmail.com>
2020-04-07 11:19:38 +0530

    Addition of required files

---
+++ lib/login_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  String _mail;
+  String _password;
+
+  void validateAndSave()
+  {
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Flutter login demo'),
+      ),
+      body: new Container(
+          padding: EdgeInsets.all(16.0),
+          child: new Form(
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                new TextFormField(
+                  decoration: new InputDecoration(labelText: "Email"),
+                ),
+                new TextFormField(
+                  decoration: new InputDecoration(labelText: "Password"),
+                  obscureText: true,
+                ),
+                new RaisedButton(
+                  child: new Text('Login', style: new TextStyle(fontSize: 20.0)),
+                  onPressed: validateAndSave,
+                )
+
+              ],
+            ),
+          )
+      )
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import "package:flutter/material.dart";
+import 'login_page.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return new MaterialApp(
+      title: "Flutter login demo",
+      theme: new ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: new LoginPage()
+    );
+  }
+}
